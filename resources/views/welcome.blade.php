d93688f65c7906fdb242ce7d4e37c65ffc38c750
quitenoisemaker <samsonojugo@gmail.com>
2023-05-01 20:29:44 +0100

    project initialization

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+            
+
+            <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+                <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
+                    <button type="submit" class="btn btn-success">Test</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
